c87077dbd713e6ff5407fda43bcc5e5b1476c3bc
Jay <57915168+JayTheferret@users.noreply.github.com>
2020-05-22 10:51:34 +0200

    correctedt postfix (to stop loop)
    changed positioning ov reverse(Xs,R2) and prefix(R2s,R1s)

--- Praktikum/Praktikum 3/Aufgabe 1.pl
+++ Praktikum/Praktikum 3/Aufgabe 1.pl
@@ -1,14 +1,14 @@
 %prefix(Xs,Ys) : Ys beginnt mit der Liste Xs.  (ÜA 20b)
     % Xs[d,e,f] Ys [d,e,f,a,b,c]
-prefix(Xs,Ys) :- append(Xs,_,Ys).
+prefix(Xs,Ys) :- append(Xs,_,Ys). %Xs + _ = Ys
 
 %postfix(Xs,Ys)  : Ys endet mit der Liste Xs. 
     % Xs[d,e,f] Ys [a,b,c,d,e,f] -> postfix 
 
-postfix(Xs,Ys) :- reverse(Ys,R1s), reverse(Xs,R2s), prefix(R2s,R1s).
+postfix(Xs,Ys) :- reverse(Ys,R1s), prefix(R2s,R1s) , reverse(Xs,R2s).
     %Ys[a,b,c,d,e,f] -> R1s[f,e,d,c,b,a]
     %Xs[d,e,f] -> R2s[f,e,d}-> praefix?
 
 %postfix(Xs,Ys) :- append(_,Xs,Ys). %mit append
 
-?- postfix([d,e,f],[a,b,c,d,e,f]).
+?- postfix([d,e,f],[a,b,c,d,e,f]).ix
